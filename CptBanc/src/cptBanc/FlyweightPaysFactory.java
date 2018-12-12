b106bb854757dfb12afa59d22cd8910b04c1a5ed
Loic MENGUY <loicm97@gmail.com>
2018-12-12 03:27:58 +0100

    misc
    misc

--- CptBanc/src/cptBanc/FlyweightPaysFactory.java
+++ CptBanc/src/cptBanc/FlyweightPaysFactory.java
@@ -6,13 +6,13 @@ public class FlyweightPaysFactory
 {
 	private static final HashMap<String, Pays> paysHash = new HashMap<String, Pays>();
 	
-	public static Pays getPays(String nom/*, int taillePop, double pib*/)
+	public static Pays getPays(String nom)
 	{
 		Pays country = (Pays)paysHash.get(nom);
 				
 		if(country == null)
 		{
-			country = new Pays(nom/*, taillePop, pib*/);
+			country = new Pays(nom);
 			paysHash.put(country.getNom(), country);
 		}
 		return country;
--- CptBanc/src/cptBanc/Pays.java
+++ CptBanc/src/cptBanc/Pays.java
@@ -17,41 +17,4 @@ public class Pays
 	{
 		this.nom = nom;
 	}
-	
-	/*private int taillePop;
-	private double pib;
-	
-	public Pays(String nom, int taillePop, double pib)
-	{
-		setNom(nom);
-		setTaillePop(taillePop);
-		setPib(pib);
-	}
-	
-	public String getNom() 
-	{
-		return nom;
-	}
-	public void setNom(String nom) 
-	{
-		this.nom = nom;
-	}
-	
-	public int getTaillePop() 
-	{
-		return taillePop;
-	}
-	public void setTaillePop(int taillePop) 
-	{
-		this.taillePop = taillePop;
-	}
-	
-	public double getPib() 
-	{
-		return pib;
-	}
-	public void setPib(double pib)
-	{
-		this.pib = pib;
-	}*/
 }
